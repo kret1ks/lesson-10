6d9efdcd3b3fa207cda9d2b8dfa0c5bbdeed5a79
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-03-12 16:04:59 +0200

    123

--- src/app.js
+++ src/app.js
@@ -1,61 +1,70 @@
 // Завдання "Таймер інтервалу": Створіть програму, яка виводить повідомлення кожну секунду за допомогою setInterval. Після 5 повідомлень зупиніть виконання інтервалу за допомогою clearInterval.
 
 
-// let count = 0
+let count = 0
 
-// const timerId = setInterval(() => {
-//     count += 1
-//     console.log("Повідомлення", count);
-//     if(count === 5){
-//         clearInterval(timerId);
-//     }
-// },1000);
+const id = setInterval(() => {
+  count += 1  
+  console.log(count);
+  if(count === 5){
+    console.log("кінець");
+    clearInterval(id)
+  }
+},1000)
+
 
 
 // Завдання "Анімація елементів": Створіть кілька елементів на сторінці і реалізуйте просту анімацію, змінюючи їх розмір, положення чи стилі через певний інтервал за допомогою setInterval.
 
 
-// const boxRef = document.querySelector(".box")
+const boxRef = document.querySelector(".box")
+const boxEl = document.querySelector(".boxed")
 
-// let position = 0
+let counts = 0;
+let position = 0;
+
+const array = ["red", "blue", "green", "orange"]
+
+setInterval(() => {
+    boxRef.style.backgroundColor = array[counts];
+    counts++;
+    if (counts === array.length) {
+        counts = 0;
+    }
+    position += 20
+    boxRef.style.left = position + "px"
+    if(position >= 2000){
+        position = 0;
+    }
+}, 150)
 
 
-// setInterval(() => {
-//     position += 2
-//     boxRef.style.left = position + "px"
-//     boxRef.style.top = position + "px"
-   
-// }, 20)
+
 
 
 
 // Завдання "Інтерактивна гра": Створіть просту інтерактивну гру, де гравець має натискати на елементи на сторінці протягом певного інтервалу часу, використовуючи setInterval. Реалізуйте лічильник очок та відслідковуйте кількість натисків гравця.
 
-// const gameRef = document.querySelector(".game");
 
-// let score = 0;
-// let timeLeft = 15;
-// let timerId = null;
+const gameRef = document.querySelector(".game");
 
-// gameRef.addEventListener("click", () => {
-//     score++;
-//     console.log("Рахунок:", score); 
-// });
+let score = 0;
+let timeLeft = 5;
 
-// timerId = setInterval(() => {
-//     timeLeft--;
-//     console.log("Залишилось часу:", timeLeft);
+gameRef.addEventListener("click", () => {
+    if (score === 0) {
+        const timerId = setInterval(() => {
+            timeLeft--;
 
-//     if (timeLeft === 0) {
-//         clearInterval(timerId); 
-//         alert("Гра закінчена! Твій результат: " + score);
-//     }
-// }, 1000);
+            if (timeLeft <= 0) {
+                clearInterval(timerId);
+                alert("Час вийшов! Твій рахунок: " + score);
+            }
+        }, 1000);
+    }
 
-
-
-
-
+    score += 1;
+});
 
 // Завдання "Контроль часу": Створіть програму, яка дозволяє користувачу встановити певний час (у секундах) за допомогою введення з клавіатури. Потім використовуйте setTimeout або setInterval, щоб після встановленого часу вивести повідомлення.
 
@@ -65,19 +74,43 @@ const timeRef = document.querySelector('#time');
 const btnRef = document.querySelector(".start");
 
 let userTime = 0; 
+// let timeoutId = null;
 
 timeRef.addEventListener("input", (event) => {
-
     userTime = Number(event.target.value);
     console.log("Встановлено час:", userTime);
 });
 
 btnRef.addEventListener("click", () => {
-    console.log("Таймер запущено на", userTime, "секунд");
+    console.log("Таймер запущено на", userTime);
     
     setTimeout(() => { 
         alert("Час вийшов!"); 
     }, userTime * 1000); 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
